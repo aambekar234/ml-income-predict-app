2ed9be6886bbe69a8e94321a84287073fc77b7a0
aambekar234 <aambekar234@gmail.com>
2023-07-28 13:29:29 -0500

    removed bugs

--- starter.sh
+++ starter.sh
@@ -5,7 +5,9 @@ mkdir keys
 ls -al
 
 cat /etc/secrets/sa.json >> keys/sa-key.json
-echo keys/sa-key.json
+cat keys/sa-key.json
+dvc remote default
 
+echo "******** Now doing dvc pull ************"
 dvc pull
 gunicorn -w 3 -k uvicorn.workers.UvicornWorker main:app
