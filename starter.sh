71da41e575256a821e23d58998599ac8f48f4e69
aambekar234 <aambekar234@gmail.com>
2023-07-28 13:21:45 -0500

    updated starter script

--- starter.sh
+++ starter.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+pip install --upgrade pip
 pip3 install -r requirements.txt
 mkdir keys
-echo "${sakey}" >> keys/sa-key.json
+ls -al
+
+cat /etc/secrets/sa.json >> keys/sa-key.json
+echo keys/sa-key.json
+
 dvc pull
 gunicorn -w 3 -k uvicorn.workers.UvicornWorker main:app
