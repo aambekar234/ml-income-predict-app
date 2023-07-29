bb13550e80f0e5d8810e63ed57080f54f5b6fb5e
aambekar234 <aambekar234@gmail.com>
2023-07-28 22:12:08 -0500

    added github actions

--- starter.sh
+++ starter.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-pip install --upgrade pip
-pip3 install -r requirements.txt
-mkdir keys
-ls -al
 
-cat /etc/secrets/sa.json >> keys/sa-key.json
-cat keys/sa-key.json
-dvc remote default
+ls -la
 
 # echo "******** Now doing dvc pull ************"
-# dvc pull
-gunicorn -w 3 -k uvicorn.workers.UvicornWorker main:app
+conda init bash
+source ~/.bashrc
+conda activate base-ml-py3.8
+dvc remote default
+dvc pull
+gunicorn -w 3 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:3100 main:app
