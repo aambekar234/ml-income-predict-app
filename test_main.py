32249ede331dc6cce76679fdc9de4a0f68abdb38
aambekar234 <aambekar234@gmail.com>
2023-07-30 20:15:28 -0500

    added extra test cases

--- test_main.py
+++ test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 
 @pytest.fixture
 def get_payload():
-    payload = {
+    return {
     "age": 50,
     "workclass": "Private",
     "fnlgt": 83311,
@@ -22,7 +22,23 @@ def get_payload():
     "capital_loss": 0,
     "hours_per_week": 80,
     "native_country": "United-States"}
-    return payload
+
+@pytest.fixture
+def get_payload2():
+    return {
+    "age": 10,
+    "workclass": "Private",
+    "fnlgt": 83311,
+    "education": 14,
+    "marital_status": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 0,
+    "capital_loss": 0,
+    "hours_per_week": 0,
+    "native_country": "United-States"}
 
 @pytest.fixture
 def get_payload_invalid():
@@ -35,14 +51,18 @@ def get_payload_invalid():
 
 def test_greeting():
     response = client.get("/")
-    print(response)
     assert response.status_code == 200
     assert response.json() == "Welcome to Income predict model inference!!"
 
-def test_inference(get_payload):
+def test_inference_1(get_payload):
     response = client.post("/predict", json=get_payload)
-    print(response)
     assert response.status_code == 200
+    assert response.json()[0] == "prediction is 1"
+
+def test_inference_0(get_payload2):
+    response = client.post("/predict", json=get_payload2)
+    assert response.status_code == 200
+    assert response.json()[0] == "prediction is 0"
 
 def test_inference_invalid_request(get_payload_invalid):
     response = client.post("/predict", json=get_payload_invalid)
