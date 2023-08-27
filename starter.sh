5cb4c6c15e791c97a90dd7c3dd14e50d73dc015f
aambekar234 <aambekar234@gmail.com>
2023-08-27 11:18:32 -0500

    workflow updated

--- starter.sh
+++ starter.sh
@@ -5,6 +5,6 @@ conda init bash
 source ~/.bashrc
 conda activate base-ml-py3.8
 # echo ******** Now doing dvc pull ************
-dvc pull
+dvc pull --force
 # echo ******** Starting the Service ************
 gunicorn -w 3 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:3100 main:app
